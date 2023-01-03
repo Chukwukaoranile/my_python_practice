d279a9c65bd5f18b9f68c86769a535c6252ac552
chukwukaoranile <prosperbinary@gmail.com>
2023-01-03 15:52:46 +0100

    My First python project

--- investment_calc.py
+++ investment_calc.py
@@ -1,7 +1,8 @@
+# A program that calculates the return on investment after 10 years
 invest = input('Enter your investment ')
 interest_rate = input('Enter interest rate ')
 invest = float(invest)
 interest_rate = float(interest_rate) * .01 # This '* .01' rounds the float to 2 decimal places
 for i in range(1, 10):
     invest = invest + (invest * interest_rate)
-    print('Return on Investment after {} year(s) {:.2f}'.format(i, invest))
+    print('Return on Investment after {} year(s) ${:.2f}'.format(i, invest))
